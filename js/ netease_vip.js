3cea5f5f0aa61e5ebf25cfc02d8f190f428d7e9f
Llealf <67813929+kiki0-zz@users.noreply.github.com>
2025-12-17 18:14:07 +0800

    更新  netease_vip.js

--- js/ netease_vip.js
+++ js/ netease_vip.js
@@ -1,1 +1,17 @@
-1
+// Netease Music VIP Clean (UI Level)
+let body = $response.body;
+
+try {
+  let obj = JSON.parse(body);
+
+  if (obj.data) {
+    obj.data.associator = {};
+    obj.data.redVip = false;
+    obj.data.musicPackage = {};
+    obj.data.redplus = false;
+  }
+
+  body = JSON.stringify(obj);
+} catch (e) {}
+
+$done({ body });
