2ba9db5e27383078f6ce56cbd06b64a05e4f0538
Llealf <67813929+kiki0-zz@users.noreply.github.com>
2025-12-17 18:15:15 +0800

    更新 netease_play.js

--- js/netease_play.js
+++ js/netease_play.js
@@ -1,1 +1,19 @@
-w
+// Netease Music Play Clean
+let body = $response.body;
+
+try {
+  let obj = JSON.parse(body);
+
+  if (obj.data) {
+    if (obj.data.needVip !== undefined) {
+      obj.data.needVip = false;
+    }
+    if (obj.data.noCopyright !== undefined) {
+      obj.data.noCopyright = false;
+    }
+  }
+
+  body = JSON.stringify(obj);
+} catch (e) {}
+
+$done({ body });
